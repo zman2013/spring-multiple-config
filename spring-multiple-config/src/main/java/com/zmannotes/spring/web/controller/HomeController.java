3a352e42a0cd1e8ca55ac7ce1aced188a91c3ff8
zman <vsmingren@qq.com>
2016-05-05 22:06:24 +0800

    init

---
+++ spring-multiple-config/src/main/java/com/zmannotes/spring/web/controller/HomeController.java
@@ -0,0 +1,29 @@
+package com.zmannotes.spring.web.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller                     // 标明此类是一个Controller
+@RequestMapping("/home")        // 标明Url根路径为/home
+public class HomeController {
+
+    @Value("${env}")
+    private String env;
+
+    @RequestMapping("hi")       // 标明Url子路径为hi
+    public String hi(
+            @RequestParam(name = "name", defaultValue = "hacker") String name,
+            Model model) {
+
+        String msg = String.format("Welcome %s, env %s", name, env);
+
+        model.addAttribute("msg", msg);
+
+        // 返回名为home的页面
+        return "home";
+    }
+
+}
---
+++ spring-multiple-config/src/main/webapp/view/home.jsp
@@ -0,0 +1,7 @@
+<html>
+<head><title>Welcome</title></head>
+
+<body>
+${msg }
+</body>
+</html>
